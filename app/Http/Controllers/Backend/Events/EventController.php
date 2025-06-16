cec796e34a6c520875540a5936c90e044a448ed0
milton_alas <ah09062@ues.edu.sv>
2025-06-16 16:38:02 -0600

    Auto stash before merge of "feature/ah09062" and "origin/main"

--- app/Http/Controllers/Backend/Events/EventController.php
+++ app/Http/Controllers/Backend/Events/EventController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers\Backend\Events;
 
-use App;
 use App\Http\Controllers\Controller;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use App\Http\Requests\Events\EventRequest; 
+use App\Http\Requests\Events\EventRequest;
 
 class EventController extends Controller
 {
@@ -20,16 +19,17 @@ class EventController extends Controller
     }
     public function index()
     {
-        $events = Event::all();
-        return view('backend.menus.index', compact('events'));
+
+        return view('backend.admin.events.events');
     }
+
 
     // Muestra la tabla de eventos del sistema
     public function eventTable(){
         $events = Event::orderBy('id', 'ASC')->get();
         return view('backend.admin.events.table.eventtable', compact('events'));
     }
-
+    
     public function create(EventRequest $request)
     {
         //Crear un Event Request para validar los datos en backend
@@ -60,11 +60,11 @@ class EventController extends Controller
             DB::beginTransaction();
             $event = Event::find($id);
 
+            // Verificar que el evento existe
             if (!$event) {
                 return ['status' => 404, 'message' => 'Evento no encontrado.'];
             }
-
-            //Datos ya validados por el EventRequest   
+            // Los datos ya están validados automáticamente por EventRequest
             $event->name = $request->event_name;
             $event->description = $request->description;
             $event->date = $request->date;
